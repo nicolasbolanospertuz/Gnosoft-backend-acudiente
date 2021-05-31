dff8a14f087723515e130fe178462857a49bac3e
unknown <ea.nielsen@uniandes.edu.co>
2021-05-31 04:18:42 -0500

    a mimir

--- app/appGnosoft/routes/users.js
+++ app/appGnosoft/routes/users.js
@@ -2,18 +2,36 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('bals');
 });
 
 /* POST users sign in */
-router.get('/signin', function(req, res, next) {
+router.get('/signin', function (req, res, next) {
   res.render('signIn.hbs');
 });
 
 /* POST users sign up */
-router.get('/register', function(req, res, next) {
+router.get('/register', function (req, res, next) {
   res.render('register.hbs');
+});
+
+router.post('/register', function (req, res, next){
+  const{name,last_name,email, pasword,confirm_password} = req.body;
+  const errors =[];
+  if(pasword != confirm_password){
+    errors.push({text:'Pasword do not match'});
+
+  } 
+  if(pasword.length < 4 ){
+    errors.push({text:' too short'});
+  }
+  if(errors.length > 0){
+    res.render('/register',{errors,name,last_name,email,pasword,confirm_password})
+  }else{
+    res.send('ok')   
+  }
+
 });
 
 module.exports = router;
